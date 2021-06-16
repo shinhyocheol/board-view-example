7262d429e5f643d5c55b6fad085fbc54bea00b8d
eXs-mobile <zzzskssk1@exs-mobile.com>
2021-06-16 17:31:18 +0900

    2021.06.16(수)  * 코드 최신화

--- src/main.js
+++ src/main.js
@@ -15,6 +15,6 @@ app.use(store);
 app.use(router);
 app.mount('#app');
 
-app.config.globalProperties.axios=axios // axios 전역사용
-app.config.globalProperties.store=store // store 전역사용
+app.config.globalProperties.axios=axios   // axios 전역사용
+app.config.globalProperties.store=store   // store 전역사용
 app.config.globalProperties.router=router // router 전역사용
