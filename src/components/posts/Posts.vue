f4d39dbaf70a1dd204fa66383182e4b1e1982e51
shinhyocheol <zzzskssk1@naver.com>
2021-07-27 22:19:09 +0900

    2021.07.27(화)  * 게시글 목록 화면 컨셉 변경에 따른 기존 테이블 화면 별도 생성

--- src/components/posts/Posts.vue
+++ src/components/posts/Posts.vue
@@ -1,55 +1,4 @@
 <template>
-  <!-- 
-    <div class="container">
-      <div class="card shadow-lg">
-        <div class="card-body">
-          <DataTable 
-            :list="list"
-            :cnt="totalCnt"
-            :getData="getPosts"
-            id="table" 
-          >
-            <template v-slot:header>
-              <th scope="col">#</th>
-              <th scope="col">제목</th>
-              <th scope="col">본문</th>
-              <th scope="col">작성자</th>
-              <th scope="col">작성일</th>
-              <th scope="col" class="text-center">비고</th>
-            </template>
-            <template v-slot:default="slotProps">
-              <td>{{slotProps.row.id}}</td>
-              <td>{{slotProps.row.title}}</td>
-              <td>{{slotProps.row.content}}</td>
-              <td>{{slotProps.row.author}}</td>
-              <td>{{slotProps.row.createdDate}}</td>
-              <td class="text-center">
-                <router-link 
-                  :to="{ 
-                    path:'/posts/detail', 
-                    query: { id: slotProps.row.id }
-                  }"
-                  class="btn btn-sm btn-primary"
-                >
-                  <i class="fa fa-search" />
-                </router-link>
-              </td>
-            </template> 
-          </DataTable>
-        </div>
-      </div>
-
-
-      <div class="row mt-3 float-right">
-        <div class="col-auto">
-          <router-link :to="{path:'/posts/reg'}"
-                        class="btn btn-primary">
-            <i class="fa fa-plus">등록</i>
-          </router-link>
-        </div>
-      </div>
-    </div> 
-  -->
   <div class="container">
     <div class="row">
       <Article
@@ -68,7 +17,6 @@
 </template>
 
 <script>
-// import DataTable from '@/components/layout/DataTable'
 import Pagination from 'v-pagination-3'
 import Article from '@/components/layout/Article'
 import api from '@/api/index.js'
@@ -82,14 +30,6 @@ export default {
       cnt : 0
     }
   },
-  // computed: {
-  //   list () {
-  //     return this.posts
-  //   },
-  //   totalCnt () {
-  //     return this.cnt
-  //   }
-  // },
   watch: {
     posts() {
       this.count = this.cnt
---
+++ src/components/posts/PostsTable.vue
@@ -0,0 +1,101 @@
+<template>
+  
+  <div class="container">
+    <div class="card shadow-lg">
+      <div class="card-body">
+        <DataTable 
+          :list="list"
+          :cnt="totalCnt"
+          :getData="getPosts"
+          id="table" 
+        >
+          <template v-slot:header>
+            <th scope="col">#</th>
+            <th scope="col">제목</th>
+            <th scope="col">본문</th>
+            <th scope="col">작성자</th>
+            <th scope="col">작성일</th>
+            <th scope="col" class="text-center">비고</th>
+          </template>
+          <template v-slot:default="slotProps">
+            <td>{{slotProps.row.id}}</td>
+            <td>{{slotProps.row.title}}</td>
+            <td>{{slotProps.row.content}}</td>
+            <td>{{slotProps.row.author}}</td>
+            <td>{{slotProps.row.createdDate}}</td>
+            <td class="text-center">
+              <router-link 
+                :to="`/posts/${slotProps.row.id}`"
+                class="btn btn-sm btn-primary"
+              >
+                <i class="fa fa-search" />
+              </router-link>
+            </td>
+          </template> 
+        </DataTable>
+      </div>
+    </div>
+
+
+    <div class="row mt-3 float-right">
+      <div class="col-auto">
+        <router-link 
+          :to="{path:'/posts/reg'}"
+          class="btn btn-primary"
+        >
+          <i class="fa fa-plus">등록</i>
+        </router-link>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import DataTable from '@/components/layout/DataTable'
+import Pagination from 'v-pagination-3'
+import api from '@/api/index.js'
+export default {
+  name: 'Posts',
+  components: {Article, Pagination},
+  data () {
+    return {
+      posts: [],
+      page: 1,
+      cnt : 0
+    }
+  },
+  computed: {
+    list () {
+      return this.posts
+    },
+    totalCnt () {
+      return this.cnt
+    }
+  },
+  watch: {
+    posts() {
+      this.count = this.cnt
+    }
+  },
+  created () {
+    this.handleService()
+  },
+  methods: {
+    handleService() {
+      var params = new URLSearchParams()
+      params.append("page", this.page)
+      this.getPosts(params)
+    },
+    getPosts(params) {
+      api.get("/posts?" + params)
+      .then(res => {
+        this.posts = res.data.content
+        this.cnt = res.data.totalElements
+      }).catch(err => {
+        alert(err)
+      })
+    }
+  }
+}
+</script>
